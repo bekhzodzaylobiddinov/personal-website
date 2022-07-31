97af3920886feb49c6e4041c311dcfa866a614cf
zokir.rakhimov@gmail.com <zokir.rakhimov@gmail.com>
2022-07-31 17:19:49 -0400

    nav border

--- components/Header.js
+++ components/Header.js
@@ -3,20 +3,22 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-zinc-800 text-zinc-400">
+    <header className="bg-zinc-800 text-zinc-400 opacity-90 border-b border-zinc-600">
       <nav className="flex px-5 space-x-5 items-center justify-end">
         <Logo />
         <Link href="#">
-          <a className="link">About</a>
+          <a className="link hover:text-lime-500 active:text-white">About</a>
         </Link>
         <Link href="#">
-          <a className="link">Experience</a>
+          <a className="link hover:text-lime-500 active:text-white">
+            Experience
+          </a>
         </Link>
         <Link href="#">
-          <a className="link">Contact</a>
+          <a className="link hover:text-lime-500 active:text-white">Contact</a>
         </Link>
         <Link href="#">
-          <a className="link">Resume</a>
+          <a className="link hover:text-lime-500 active:text-white">Resume</a>
         </Link>
       </nav>
     </header>

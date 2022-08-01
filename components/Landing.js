c78bc8562f75ef2d9cc10571f6bc3237217afa8a
zokir.rakhimov@gmail.com <zokir.rakhimov@gmail.com>
2022-08-01 12:28:58 -0400

    add transition to landing

--- components/Landing.js
+++ components/Landing.js
@@ -4,9 +4,9 @@ export default function Landing() {
   return (
     <>
       <div className="flex sm:flex-row flex-col justify-center items-center  mt-[5rem] space-x-5 whitespace-nowrap">
-        <div>
+        <div className="">
           <Image
-            className="object-cover rounded-full"
+            className="object-cover rounded-full "
             src={"/portrait.png"}
             width={150}
             height={150}
@@ -14,7 +14,7 @@ export default function Landing() {
           />
         </div>
         <div>
-          <div className="flex flex-col justify-center items-center rounded dark:text-white">
+          <div className="flex flex-col justify-center items-center rounded dark:text-white transition duration-300">
             <h1 className="text-[3rem]">Zokir Rakhimov</h1>
             <p className="">
               SOFTWARE DEVELOPER <span className="text-purple-500">&&</span>{" "}

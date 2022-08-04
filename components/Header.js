835cdceb2a08ffc68dfc22689f50ab54639f8354
zokir.rakhimov@gmail.com <zokir.rakhimov@gmail.com>
2022-08-03 22:28:24 -0400

    Removed <a> use <Link /> instead

--- components/Header.js
+++ components/Header.js
@@ -20,16 +20,15 @@ export default function Header() {
     <header className="transition duration-300 backdrop-blur-md bg-white/30  text-zinc-600 dark:bg-zinc-800/30 dark:text-zinc-400 border-b dark:border-zinc-600">
       <nav className="flex space-x-5 items-center justify-center">
         <Logo />
-        <Link href="/blog">
-          <a
-            className={
-              currentRoute.includes('/blog')
-                ? 'active link hover:text-black dark:hover:text-white '
-                : 'link hover:text-black dark:hover:text-white'
-            }
-          >
-            Blog
-          </a>
+        <Link
+          href="/blog"
+          className={
+            currentRoute.includes('/blog')
+              ? 'active link hover:text-black dark:hover:text-white '
+              : 'link hover:text-black dark:hover:text-white'
+          }
+        >
+          Blog
         </Link>
         <div className="flex items-center justify-center transition duration-300">
           <button
